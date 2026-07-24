4c50ceaf2b142208f733c5c9ffb24d05b26c3b62
xonnyyys <oliverttx@gmail.com>
2026-07-24 17:26:08 -0400

    feat(frontend): configurar TanStack Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import { configureAuthInterceptor } from './services/authInterceptor'
 import { AuthProvider } from './contexts/AuthProvider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
 
 configureAuthInterceptor()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
